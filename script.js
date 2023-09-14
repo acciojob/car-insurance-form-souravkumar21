c66e3e74508226d39189ebc99e3f4c2a21d40ec9
souravkumar21 <139917301+souravkumar21@users.noreply.github.com>
2023-09-14 20:43:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your code here
+document.getElementById('registrationForm').addEventListener('submit', function (e) {
+    e.preventDefault(); // Prevent the default form submission
+    // Construct the URL and redirect the user
+    var baseUrl = "YOUR_BASE_URL_HERE";
+    var redirectUrl = baseUrl + "/main.html";
+    window.location.href = redirectUrl;
+});
